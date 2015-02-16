971cfe9467d8b8a379592da05103fb649ffb033a
RBY <sook@eircom.net>
2015-02-16 16:57:50 +0000

    google+ added

--- app/src/main/java/com/example/rubi/projectsgs/LoginActivity.java
+++ app/src/main/java/com/example/rubi/projectsgs/LoginActivity.java
@@ -3,7 +3,7 @@ package com.example.rubi.projectsgs;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
-import android.app.Activity;
+
 import android.app.LoaderManager.LoaderCallbacks;
 import android.content.ContentResolver;
 import android.content.CursorLoader;
@@ -11,7 +11,7 @@ import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
-import android.os.Build.VERSION;
+
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
@@ -26,14 +26,23 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.SignInButton;
+
 import java.util.ArrayList;
 import java.util.List;
 
 
 /**
- * A login screen that offers login via email/password.
+ * A login screen that offers login via email/password and via Google+ sign in.
+ * <p/>
+ * ************ IMPORTANT SETUP NOTES: ************
+ * In order for Google+ sign in to work with your app, you must first go to:
+ * https://developers.google.com/+/mobile/android/getting-started#step_1_enable_the_google_api
+ * and follow the steps in "Step 1" to create an OAuth 2.0 client for your package.
  */
-public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
+public class LoginActivity extends PlusBaseActivity implements LoaderCallbacks<Cursor> {
 
     /**
      * A dummy authentication store containing known user names and passwords.
@@ -51,12 +60,32 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
     private AutoCompleteTextView mEmailView;
     private EditText mPasswordView;
     private View mProgressView;
+    private View mEmailLoginFormView;
+    private SignInButton mPlusSignInButton;
+    private View mSignOutButtons;
     private View mLoginFormView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Find the Google+ sign in button.
+        mPlusSignInButton = (SignInButton) findViewById(R.id.plus_sign_in_button);
+        if (supportsGooglePlayServices()) {
+            // Set a listener to connect the user when the G+ button is clicked.
+            mPlusSignInButton.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    signIn();
+                }
+            });
+        } else {
+            // Don't offer G+ sign in if the app's version is too low to support Google Play
+            // Services.
+            mPlusSignInButton.setVisibility(View.GONE);
+            return;
+        }
 
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
@@ -84,16 +113,12 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
+        mEmailLoginFormView = findViewById(R.id.email_login_form);
+        mSignOutButtons = findViewById(R.id.plus_sign_out_buttons);
     }
 
     private void populateAutoComplete() {
-        if (VERSION.SDK_INT >= 14) {
-            // Use ContactsContract.Profile (API 14+)
-            getLoaderManager().initLoader(0, null, this);
-        } else if (VERSION.SDK_INT >= 8) {
-            // Use AccountManager (API 8+)
-            new SetupEmailAutoCompleteTask().execute(null, null);
-        }
+        getLoaderManager().initLoader(0, null, this);
     }
 
 
@@ -197,6 +222,62 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
     }
 
     @Override
+    protected void onPlusClientSignIn() {
+        //Set up sign out and disconnect buttons.
+        Button signOutButton = (Button) findViewById(R.id.plus_sign_out_button);
+        signOutButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signOut();
+            }
+        });
+        Button disconnectButton = (Button) findViewById(R.id.plus_disconnect_button);
+        disconnectButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                revokeAccess();
+            }
+        });
+    }
+
+    @Override
+    protected void onPlusClientBlockingUI(boolean show) {
+        showProgress(show);
+    }
+
+    @Override
+    protected void updateConnectButtonState() {
+        //TODO: Update this logic to also handle the user logged in by email.
+        boolean connected = getPlusClient().isConnected();
+
+        mSignOutButtons.setVisibility(connected ? View.VISIBLE : View.GONE);
+        mPlusSignInButton.setVisibility(connected ? View.GONE : View.VISIBLE);
+        mEmailLoginFormView.setVisibility(connected ? View.GONE : View.VISIBLE);
+    }
+
+    @Override
+    protected void onPlusClientRevokeAccess() {
+        // TODO: Access to the user's G+ account has been revoked.  Per the developer terms, delete
+        // any stored user data here.
+    }
+
+    @Override
+    protected void onPlusClientSignOut() {
+
+    }
+
+    /**
+     * Check if the device supports Google Play Services.  It's best
+     * practice to check first rather than handling this as an error case.
+     *
+     * @return whether the device supports Google Play Services
+     */
+    private boolean supportsGooglePlayServices() {
+        return GooglePlayServicesUtil.isGooglePlayServicesAvailable(this) ==
+                ConnectionResult.SUCCESS;
+    }
+
+    @Override
     public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
         return new CursorLoader(this,
                 // Retrieve data rows for the device user's 'profile' contact.
@@ -240,35 +321,6 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
         int IS_PRIMARY = 1;
     }
 
-    /**
-     * Use an AsyncTask to fetch the user's email addresses on a background thread, and update
-     * the email text field with results on the main UI thread.
-     */
-    class SetupEmailAutoCompleteTask extends AsyncTask<Void, Void, List<String>> {
-
-        @Override
-        protected List<String> doInBackground(Void... voids) {
-            ArrayList<String> emailAddressCollection = new ArrayList<String>();
-
-            // Get all emails from the user's contacts and copy them to a list.
-            ContentResolver cr = getContentResolver();
-            Cursor emailCur = cr.query(ContactsContract.CommonDataKinds.Email.CONTENT_URI, null,
-                    null, null, null);
-            while (emailCur.moveToNext()) {
-                String email = emailCur.getString(emailCur.getColumnIndex(ContactsContract
-                        .CommonDataKinds.Email.DATA));
-                emailAddressCollection.add(email);
-            }
-            emailCur.close();
-
-            return emailAddressCollection;
-        }
-
-        @Override
-        protected void onPostExecute(List<String> emailAddressCollection) {
-            addEmailsToAutoComplete(emailAddressCollection);
-        }
-    }
 
     private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
         //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
